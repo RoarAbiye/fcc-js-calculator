0a1f04c9f39ad30d1616f64243bd7d3816fd4604
robelabiye <rblabiye5@gmail.com>
2022-01-14 23:33:15 +0300

    created calculator logic

--- src/App.js
+++ src/App.js
@@ -2,27 +2,12 @@ import './App.css';
 import React from 'react';
 import ReactFCCtest from 'react-fcctest';
 
-const buttonId  = {
-            "1": "one",
-            "2": "two",
-            "3": "three",
-            "4": "four",
-            "5": "five",
-            "6": "six",
-            "7": "seven",
-            "8": "eight",
-            "9": "nine",
-            "0": "zero",
-            ".": "decimal",
-        }
-
-
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {currentOperand: "", 
                       prevOpperand: "",
-                      opperation: null
+                      opperation: false
         }
         
         this.dispatch = this.dispatch.bind(this);
@@ -32,41 +17,83 @@ class App extends React.Component {
         this.dispatch(action)
     };
     
-    /*    oppButtonHandler = (opp) => {
-        this.setState({prevOpperand: this.state.currentOperand, currentOperand: "" })
-    }
-    */
     dispatch = action => {
         //shorthand for state variables
     const C_OP = this.state.currentOperand;
-    const P_OP = this.state.prevOpperand;
-    console.log(action)
     switch(action.type) {
         case "ADD_DIGIT":
-            this.setState({currentOperand: C_OP + action.digit});
+                    this.setState({currentOperand: C_OP + action.payload})
+                
             break;
         case "DELET":
             this.setState({currentOperand: C_OP.slice(0,-1)});
             break;
-        case "EVALUATE":
-        //    this.setState({currentOperand: C_OP.slice(0,-1)});
-            break;
-        case "ADD":
-         //   this.setState({currentOperand: C_OP.slice(0,-1)});
-            break;
-        case "SUBTRACT":
-          //  this.setState({currentOperand: C_OP.slice(0,-1)});
-            break;
-        case "DIVIDE":
-          //  this.setState({currentOperand: C_OP.slice(0,-1)});
-            break;
-        case "MULTIPLY":
-          //  this.setState({currentOperand: C_OP.slice(0,-1)});
+        case "CALCULATE":
+            this.calculator(action.payload)
             break;
         default:
             return
     }}
+    
+    calculator = (operator) => {
+        switch(operator) {
+            case 'ADD':
+                this.setState({ prevOpperand: this.state.currentOperand, 
+                                currentOperand: "", 
+                                opperation: "ADD"
+                              }) 
+                break
+            case 'SUBTRACT':
+                this.setState({ prevOpperand: this.state.currentOperand, 
+                                currentOperand: "", 
+                                opperation: 'SUBTRACT'
+                              }) 
+                break
+            case 'DIVIDE':
+                this.setState({ prevOpperand: this.state.currentOperand, 
+                                currentOperand: "", 
+                                opperation: "DIVIDE"
+                              }) 
+                break
+            case 'MULTIPLY':
+                this.setState({ prevOpperand: this.state.currentOperand, 
+                                currentOperand: "", 
+                                opperation: "MULTIPLY"
+                              }) 
+                break
+            default:
+                return
+        }
+    }
+    
+    evaluate = () => {
+        const float_C_OP = parseFloat(this.state.currentOperand)
+        const float_P_OP = parseFloat(this.state.prevOpperand)
+        const opperation = this.state.opperation;
+        let result;
 
+        switch(opperation){
+           case 'ADD':
+               result = float_C_OP + float_P_OP;
+               this.setState({currentOperand: result})
+               break
+           case 'SUBTRACT':
+               result = float_C_OP - float_P_OP;
+               this.setState({currentOperand: result})
+               break
+           case 'MULTIPLY':
+               result = float_C_OP * float_P_OP;
+               this.setState({currentOperand: result})
+               break
+           case 'DIVIDE':
+               result = float_C_OP / float_P_OP;
+               this.setState({currentOperand: result})
+               break
+           default:
+               return
+        }
+
+    }
 render() {
     return (
         <div>
@@ -83,25 +110,25 @@ render() {
                                         }
             >AC</button>
 
-            <button id="delet" onClick={()=> {this.dispatch({type: "DELET", digit: ""})}} >C</button>
+            <button id="delet" onClick={()=> {this.dispatch({type: "DELET", payload: ""})}} >C</button>
 
-            <button id = "one"    onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '1'})}}>1</button>
-            <button id = "two"    onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '2'})}}>2</button>
-            <button id = "three"  onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '3'})}}>3</button>
-            <button id = "four"   onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '4'})}}>4</button>
-            <button id = "five"   onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '5'})}}>5</button>
-            <button id = "six"    onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '6'})}}>6</button>
-            <button id = "seven"  onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '7'})}}>7</button>
-            <button id = "eight"  onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '8'})}}>8</button>
-            <button id = "nine"   onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '9'})}}>9</button>
-            <button id = "zero"   onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '0'})}}>0</button>
-            <button id = "decimal"onClick={() => {this.dispatch({type: "ADD_DIGIT", digit: '.'})}}>.</button>
+            <button id = "one"    onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '1'})}}>1</button>
+            <button id = "two"    onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '2'})}}>2</button>
+            <button id = "three"  onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '3'})}}>3</button>
+            <button id = "four"   onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '4'})}}>4</button>
+            <button id = "five"   onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '5'})}}>5</button>
+            <button id = "six"    onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '6'})}}>6</button>
+            <button id = "seven"  onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '7'})}}>7</button>
+            <button id = "eight"  onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '8'})}}>8</button>
+            <button id = "nine"   onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '9'})}}>9</button>
+            <button id = "zero"   onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '0'})}}>0</button>
+            <button id = "decimal"onClick={() => {this.dispatch({type: "ADD_DIGIT", payload: '.'})}}>.</button>
 
-            <button id="equals"   onClick={() => {this.oppButtonHandler("EVALUATE")}} >=</button>
-            <button id="add"      onClick={() => {this.oppButtonHandler("ADD")}}  >+</button>
-            <button id="subtract" onClick={() => {this.oppButtonHandler("SUBTRACT")}}  >-</button>
-            <button id="divide"   onClick={() => {this.oppButtonHandler("DIVIDE")}}  >/</button>
-            <button id="multiply" onClick={() => {this.oppButtonHandler("MULTIPLY")}}  >*</button>
+            <button id="equals"      onClick={this.evaluate} >=</button>
+            <button id="add"         onClick={() => {this.dispatch({type: "CALCULATE", payload: "ADD"})}}       >+</button>
+            <button id="subtract"    onClick={() => {this.dispatch({type: "CALCULATE", payload: "SUBTRACT"})}}  >-</button>
+            <button id="multiply"    onClick={() => {this.dispatch({type: "CALCULATE", payload: "MULTIPLY"})}}  >*</button>
+            <button id="divide"      onClick={() => {this.dispatch({type: "CALCULATE", payload: "DIVIDE"})}}    >/</button>
         </div>
         </div>
     );
